68434439c8c051a2400cab0a410f1afe12ee39db
Jagath Samarabandu <jagath@uwo.ca>
2019-11-23 13:05:42 -0500

    Implemented POST functionality. Cleaned up console log msgs

--- index.js
+++ index.js
@@ -36,35 +36,58 @@ router.use(express.json());
 
 router.get('/:id', function(req, res) {
 	Part.findOne({id: req.params.id}, function (err, part) {
-		console.log('ID: ' + req.params.id + ' Part: ' + part);
+		console.log(`Found part ${req.params.id} with values: ${part}`);
 		if (err)
-			res.send(err);
-		res.json(part);
+			res.status(500).send(err);
+		else
+			res.json(part);
 	});
 });
 
 // POST requires a parameter for now
 router.post('/:id', function(req, res) {
-	console.log('Data: ' + JSON.stringify(req.body));
-	
-	// merge existing object with given object using Object.assign()
-	let data = Object.assign(db.get(req.params.id), req.body);
-	db.set(req.params.id, data); // save the merged object
-  res.send(JSON.stringify(data)); // return merged object
+	console.log(`POST data: ${JSON.stringify(req.body)}`);
+	Part.findOne({id: req.params.id}, function (err, part) {
+		if (err) { // server error
+			console.log(`Error while searching for part ${req.params.id}: ${err}`);
+			res.status(500).send(err);
+		} else { // found it!
+			console.log(`Found part ${req.params.id} with values: ${part}`);
+			if (part === null) {
+				console.log(`Part ${req.params.id} not found. Creating new part`);
+				part = {id: req.params.id}; // make a new array if part not found
+			}
+			// combine new and old parts and merge it with a new Part model
+			let newpart = Object.assign(new Part(), part, req.body);
+			console.log(`Part name:${newpart.name}, qty: ${newpart.qty}, id: ${newpart.id}`);
+			newpart.save(function(err) {		// Save the part in database
+				if (err) {	// server error
+					console.log(`Save error part: ${req.params.id} - ${err}`);
+					res.status(500).send(err);
+				} else {
+					res.json({ message: `Part updated: ${newpart.name}` });
+					console.log(`Part updated: ${newpart.name}`);
+				}
+			});		
+		}
+	});
 });
 
 router.put('/:id', function(req, res) {
-	console.log('Data: ' + JSON.stringify(req.body));
+	console.log(`PUT Data: ${JSON.stringify(req.body)}`);
 	var part = new Part();
-	part.name = req.body.name;
+	part.name = req.body.name;		// Get part properties from submitted data
 	part.qty = req.body.qty;
-	part.id = req.params.id
-	part.save(function(err) {
-		if (err)
-			res.send(err);
-		res.json({ message: 'Part created! ' + part.name });
-		console.log("Part created: " + part.name);
-	})
+	part.id = req.params.id			// Part ID is from URL parameter
+	part.save(function(err) {		// Save the part in database
+		if (err) {	// server error
+			console.log(`Save error part: ${req.params.id} - ${err}`);
+			res.status(500).send(err);
+		} else {
+			res.json({ message: `Part created: ${part.name}` });
+			console.log(`Part created: ${part.name}`);
+		}
+	});
 });
 
 app.use('/api', router); // Set the routes at '/api'
